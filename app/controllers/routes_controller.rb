ac01d3e22de75648a413a21c028023d71a288e8a
Lukas Klappert <lklappert.mmt-b2013@fh-salzburg.ac.at>
2015-01-03 17:28:51 +0100

    replaced Route.new by Route.find_or_create_by

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,7 +1,7 @@
 class RoutesController < ApplicationController
 
   def get_json
-    @route = Route.new(origin: params[:origin], destination: params[:destination])
+    @route = Route.find_or_create_by(origin: params[:origin], destination: params[:destination])
 
     respond_to do |format|
       format.json { render :json => @route.to_json(:except => [:created_at,:updated_at])}
