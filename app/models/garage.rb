d35b1d8d946bdeeead35d2a4c31fed5961d796cc
Lukas Klappert <lklappert.mmt-b2013@fh-salzburg.ac.at>
2015-01-07 17:34:56 +0100

    added garage#update_data method

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -1,2 +1,18 @@
 class Garage < ActiveRecord::Base
+
+  def update_data ( json, type )
+    # if self.name == nil
+    # name setzen
+      if(type == 'DIE')
+        self.price_die = json["spritPrice"][0]["amount"].to_f
+      elsif(type == 'SUP')
+        self.price_sup = json["spritPrice"][0]["amount"].to_f
+      end
+
+      self.description = json["address"]
+
+      self.save
+
+  end
+
 end
