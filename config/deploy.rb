cde6c87a1f5a7ede834924023c75cceda65c5976
Simon Hintersonnleitner <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2015-01-24 14:18:45 +0100

    try to fix remote databse

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :deploy_via, :copy
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+ set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
