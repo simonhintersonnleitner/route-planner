5eb4ddb4e45c3fe8532ff15db88dcfd22e651d8a
Simon Hintersonnleitner <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2015-01-26 18:23:42 +0100

    refactoring

--- app/models/price_data.rb
+++ app/models/price_data.rb
@@ -56,6 +56,74 @@ class PriceData < ActiveRecord::Base
     return count.to_s + " datasets deleted"
   end
 
+  def self.save_price_for_all_cities
+    count = 0
+    ReferenceCities.all().each do |city|
+      save_price_of_city(city)
+      count += 1
+    end
+
+    return count.to_s + " datasets added"  
+  end
+
+  def self.save_price_of_city(city)
+  
+    #render :text => decode1[0]["errorItems"][0]["msgText"]
+
+    response_diesel = get_response_from_api(city,"DIE")
+    response_super = get_response_from_api(city,"SUP")
+
+    insert(city,get_min(response_diesel),get_min(response_super),get_average(response_diesel),get_average(response_super),get_max(response_diesel),get_max(response_super))
+
+  end
+
+  def self.get_response_from_api(city,type)
+    uri = URI('http://www.spritpreisrechner.at/espritmap-app/GasStationServlet')
+
+    lng_north_east = city.lng_north_east
+    lat_north_east = city.lat_north_east
+
+    lng_south_west = city.lng_south_west
+    lat_south_west = city.lat_south_west
+    
+
+    respone = Net::HTTP.post_form(uri, "data" => "['','#{type}','#{lng_north_east}','#{lat_north_east}','#{lng_south_west}','#{lat_south_west}']")
+
+    return ActiveSupport::JSON.decode(respone.body)
+
+  end
+
+  def self.get_average(respone)
+    sum = 0
+
+    for i in 0..4
+      sum += respone[i]["spritPrice"][0]["amount"].to_f
+    end
+
+    sum /= 5
+
+    return sum.round(3)
+  end
+
+  def self.get_min(respone)
+    return respone[0]["spritPrice"][0]["amount"].to_f
+  end
+
+  def self.get_max(respone)
+    return respone[4]["spritPrice"][0]["amount"].to_f
+  end
+
+  def self.insert(city,min_diesel,min_super,average_diesel,average_super,max_diesel,max_super)
+    priceDataset = PriceData.new
+    priceDataset.city_fk = city.id
+    priceDataset.min_diesel = min_diesel
+    priceDataset.min_super = min_super
+    priceDataset.average_diesel = average_diesel
+    priceDataset.average_super = average_super
+    priceDataset.max_diesel = max_diesel
+    priceDataset.max_super = max_super
+    priceDataset.save!
+  end
 end
 
 
